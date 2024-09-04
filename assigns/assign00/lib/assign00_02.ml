09626c0bfe2034f0d078bd6ed75c29889a3e9007
Alvin Z <alvinzhu65@gmail.com>
2024-09-04 18:08:20 -0400

    finished p1/p2

---
+++ assigns/assign00/lib/assign00_02.ml
@@ -0,0 +1,8 @@
+let is_prime n =
+  let rec check_divisors d =
+    if d * d > n then true
+    else if n mod d = 0 then false
+    else check_divisors (d + 1)
+  in
+  if n < 2 then false
+  else check_divisors 2
