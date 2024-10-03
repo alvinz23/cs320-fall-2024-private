9ab91c2d3d8dc1aca23d40f2bbc7325d6a83765b
Alvin Z <alvinzhu65@gmail.com>
2024-10-03 02:38:12 -0400

    finished q1

--- assigns/assign04/lib/assign04_01.ml
+++ assigns/assign04/lib/assign04_01.ml
@@ -1,39 +1,51 @@
 
-open Stdlib320
 
+(* Corrected solve function *)
+(* Your solve function remains the same *)
 let solve funcs start pred = 
   let rec wrap start funcs pred steps limit = 
-    if steps > limit  then 
+    if steps > limit then 
       (None, funcs) 
-  else if pred start then (Some steps, funcs)
-  else let newVal = funcs start in wrap newVal funcs pred (steps + 1) limit
+    else if pred start then (Some steps, funcs)
+    else 
+      let newVal = funcs start in 
+      wrap newVal funcs pred (steps + 1) limit
+  in wrap start funcs pred 0 950
 
-in wrap start funcs pred 0 950
-
-let rec checkDuplicate maxVal funcs count infCount start pred= 
+(* Corrected checkDuplicate function *)
+let rec checkDuplicate maxVal funcs count infCount start pred = 
   match funcs with 
   | [] -> count < 2 && infCount < 2
-  | h :: t -> let (steps, _) = solve h start pred
-in match steps with 
-  | None -> if (infCount + 1) = 2 then false else checkDuplicate maxVal t count (infCount + 1) start pred 
-  | Some s when s = maxVal -> if (count + 1) = 2 then false else checkDuplicate maxVal t (count + 1) infCount start pred
-  | Some _ -> checkDuplicate maxVal t count infCount start pred
+  | h :: t -> 
+      let (steps, _) = solve h start pred in
+      match steps with 
+      | None -> 
+          if (infCount + 1) = 2 then false 
+          else checkDuplicate maxVal t count (infCount + 1) start pred 
+      | Some s when s = maxVal -> 
+          if (count + 1) = 2 then false 
+          else checkDuplicate maxVal t (count + 1) infCount start pred
+      | Some _ -> 
+          checkDuplicate maxVal t count infCount start pred
 
 
-  let last_function_standing funcs start pred =
-    if List.length funcs = 0 then None 
-    else
+let last_function_standing funcs start pred =
+  if List.length funcs = 0 then None 
+  else
+    let orig_funcs = funcs in 
     let rec helpSolve funcs maxVal maxFunc = 
       match funcs with 
-      | [] -> if checkDuplicate maxVal funcs 0 0 start pred then Some maxFunc else None  (* Base case: no more functions to check *)
+      | [] -> 
+          if checkDuplicate maxVal orig_funcs 0 0 start pred then Some maxFunc else None
       | h :: t -> 
-          let (steps, func) = solve h start pred in  (* Call solve on the current function *)
+          let (steps, func) = solve h start pred in 
           match steps with
-          | None ->  (* Handle infinite lifespan *)
-              if checkDuplicate maxVal funcs 0 0 start pred then Some func else None
-          | Some s when s > maxVal ->  (* Update maxVal if current function has longer lifespan *)
-              helpSolve t s func  (* Call helpSolve recursively on the rest, using 's' as maxVal *)
+          | None -> 
+              helpSolve t maxVal maxFunc  
+          | Some s when s > maxVal ->  
+              helpSolve t s func  
+          | Some s when s = maxVal ->  
+              helpSolve t maxVal maxFunc
           | Some _ ->  
-              helpSolve t maxVal maxFunc  (* Continue with the rest of the list without updating maxVal *)
-    in helpSolve funcs 0 (fun x -> x)  (* Initial call with default maxVal of 0 and a default identity function for maxFunc *)
-  
+              helpSolve t maxVal maxFunc  
+    in helpSolve funcs (-1) (fun x -> x) 
